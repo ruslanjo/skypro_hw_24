1a8400580fecbd17a9efbe12e94932645c92d282
ruslanjo <tarutin.ruslan99@gmail.com>
2022-08-22 11:40:40 +0300

    regex func v2

--- utils.py
+++ utils.py
@@ -1,36 +1,37 @@
-from typing import List
+from typing import Callable, Optional
 import os
+import re
 
 
 class FileHandler:
-    def __init__(self, data_directory):
+    def __init__(self, data_directory: str) -> None:
         self.data_directory = data_directory
 
-    def read_file(self, file_name: str):
-        file_path = self._append_file_name_to_path(file_name)
+    def read_file(self, file_name: str) -> list[str]:
+        file_path: str = self._append_file_name_to_path(file_name)
         with open(file_path, 'r') as file:
             return file.readlines()
 
-    def _append_file_name_to_path(self, file_name: str):
+    def _append_file_name_to_path(self, file_name: str) -> str:
         return os.path.join(self.data_directory, file_name)
 
 
 class LogQuery:
-    def filter(self, logs_data: List[str], filter_condition: str) -> list:
+    def filter(self, logs_data: list[str], filter_condition: str) -> list[str]:
 
         result = filter(lambda log: filter_condition in log, logs_data)
         return list(result)
 
-    def map(self, logs_data: List[str], column_number: str) -> list:
-        column_number = int(column_number)
-        result = map(lambda log: log.split(' ')[column_number], logs_data)
+    def map(self, logs_data: list[str], column_number: str) -> list[str]:
+        column_number_int = int(column_number)
+        result = map(lambda log: log.split(' ')[column_number_int], logs_data)
         return list(result)
 
-    def unique(self, logs_data: List[str]) -> list:
-        unique_logs = set(logs_data)
+    def unique(self, logs_data: list[str]) -> list[str]:
+        unique_logs: set = set(logs_data)
         return list(unique_logs)
 
-    def sort(self, logs_data: List[str], val) -> list:
+    def sort(self, logs_data: list[str], val: str) -> list[str]:
         if val not in ['asc', 'desc']:
             raise ValueError('sort values must be either "asc" or "desc"')
 
@@ -39,15 +40,25 @@ class LogQuery:
         else:
             is_reversed = False
 
-        result = sorted(logs_data, reverse=is_reversed)
+        result: list = sorted(logs_data, reverse=is_reversed)
 
         return result
 
-    def limit(self, logs_data: List[str], number_of_records: str):
-        number_of_records = int(number_of_records)
-        return logs_data[:number_of_records]
+    def limit(self, logs_data: list[str], number_of_records: str):
+        number_of_records_int = int(number_of_records)
+        return logs_data[:number_of_records_int]
 
-    def use_query_method(self, logs_data: List[str], command: str, value: str, available_commands: dict):
+    def regex(self, logs_data: list[str], regex: str) -> list[str]:
+        res = []
+
+        for log in logs_data:
+            res.extend(re.findall(regex, log))
+
+        return res
+
+    def use_query_method(self, logs_data: list[str], command: str, value: str, available_commands: dict):
         # Получаем и применяем объект метода, который выполняет требуемую трансформацию данных
-        query_method = available_commands.get(command)
-        return query_method(logs_data, value)
+        query_method: Optional[Callable] = available_commands.get(command)
+
+        if query_method:
+            return query_method(logs_data, value)

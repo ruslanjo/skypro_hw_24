14a61386626e174794fca81c247950cdd34db6ce
ruslanjo <103440686+ruslanjo@users.noreply.github.com>
2022-08-22 11:43:06 +0300

    research code deleted

--- container.py
+++ container.py
@@ -3,16 +3,3 @@ from config import DATA_DIR
 
 file_handler = FileHandler(DATA_DIR)
 log_query = LogQuery()
-
-
-print(type(log_query.regex))
-# data = file_handler.read_file('apache_logs.txt')
-#
-# data = log_query.regex(data, "POST")
-# data = log_query.map(data, 0)
-# data = log_query.unique(data)
-# data = log_query.sort(data, "desc")
-# print(data)
-
-# data = filter(lambda log: "POST" in log, data)
-# print(data)
